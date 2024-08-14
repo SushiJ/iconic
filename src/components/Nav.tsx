5faa245a44f857e34d1b048405b61ce54d608e41
Sushant Sharma <sushant.wurk@gmail.com>
2024-08-14 22:35:00 +0530

    Nav auth button init

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,12 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+
 import { cn } from "~/lib/utils";
 
 export default function Navbar() {
   const pathname = usePathname();
+  const { data: sessionData } = useSession();
 
   return (
     <nav className="sticky top-0 z-50 mx-auto flex h-20 w-full items-center rounded-b-xl border-black bg-black/95 px-4 text-neutral-200 backdrop-blur supports-[backdrop-filter]:bg-black/90">
@@ -37,21 +40,18 @@ export default function Navbar() {
           >
             generate
           </Link>
-          {/* <Link */}
-          {/*   href="/about" */}
-          {/*   className={cn( */}
-          {/*     "transition-colors hover:text-neutral-200/80", */}
-          {/*     pathname === "/about" */}
-          {/*       ? "text-neutral-200" */}
-          {/*       : "text-neutral-200/60", */}
-          {/*   )} */}
-          {/* > */}
-          {/*   about */}
-          {/* </Link> */}
         </ul>
       </div>
+      <p className="me-2">
+        {sessionData ? sessionData.user.name?.split(" ")[0] : ""}
+      </p>
       <div>
-        <p>Sign in</p>
+        <button
+          className="rounded-full bg-white/10 px-10 py-3 font-semibold text-black no-underline transition hover:bg-white/20"
+          onClick={sessionData ? () => void signOut() : () => void signIn()}
+        >
+          {sessionData ? "Sign out" : "Sign in"}
+        </button>
       </div>
     </nav>
   );
