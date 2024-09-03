1fc05d170124f9b26140c9ad20bc705dba0432f4
Sushant Sharma <sushant.wurk@gmail.com>
2024-09-03 22:18:15 +0530

    Feat. hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,44 @@
-import { api } from "~/utils/api";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
-function Main() {
-  return (
-    <div className="grid grid-cols-2">
-      <div>
-        <p>Something Something AI here</p>
-      </div>
-      <div>
-        <p>Something Something Photo here</p>
-      </div>
-    </div>
-  );
-}
+import { Button } from "~/components/ui/button";
 
 export default function Index() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
+  const router = useRouter();
+  const { data: sessionData } = useSession();
+  function handleClick() {
+    if (sessionData && sessionData.user) {
+      router.push("/generate");
+    } else {
+      router.push("/api/auth/signin");
+    }
+  }
   return (
     <>
-      <Main />
-      <p>{hello.data?.greeting}</p>
+      <div className="my-auto grid h-full grid-cols-2 place-items-center">
+        <div className="space-y-6">
+          <h1 className="text-6xl leading-tight">
+            Generate icons with the help of AI
+          </h1>
+          <p className="max-w-96 text-lg">
+            Use ai to generate icons for you, get you business an identity
+            quickly
+          </p>
+          <Button variant="default" onClick={handleClick}>
+            Get started
+          </Button>
+        </div>
+        <div>
+          <img
+            src="./hero_image_white.jpg"
+            style={{
+              transform: "scaleX(-1)",
+              width: "250",
+              height: "250",
+            }}
+          />
+        </div>
+      </div>
     </>
   );
 }
