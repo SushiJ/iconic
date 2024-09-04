101db277d93cb8a2cda294c25a659122a1321b5e
Sushant Sharma <sushant.wurk@gmail.com>
2024-09-04 22:40:16 +0530

    userinfo route

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,6 +1,6 @@
-import { eq } from "drizzle-orm";
+import { desc, eq } from "drizzle-orm";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
-import { users } from "~/server/db/schema";
+import { users, icons } from "~/server/db/schema";
 
 export const userRouter = createTRPCRouter({
   getUserCredits: protectedProcedure.query(async ({ ctx }) => {
@@ -9,5 +9,19 @@ export const userRouter = createTRPCRouter({
       where: eq(users.id, userId),
     });
     return user?.credits;
+  }),
+  getUserInfo: protectedProcedure.query(async ({ ctx }) => {
+    const user = await ctx.db.query.users.findFirst({
+      where: eq(users.id, ctx.session.user.id),
+    });
+    const userIcons = await ctx.db.query.icons.findMany({
+      where: eq(icons.userId, ctx.session.user.id),
+      limit: 10,
+      orderBy: desc(icons.createdAt),
+    });
+    return {
+      userIcons,
+      user,
+    };
   }),
 });
