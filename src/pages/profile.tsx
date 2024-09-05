2274f208c610fb699dbc89f2d3ff72758e531b89
Sushant Sharma <sushant.wurk@gmail.com>
2024-09-06 01:18:20 +0530

    profile page initial

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,8 +1,18 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import NotAuthenticated from "~/components/NotAuthenticated";
+
 import { api } from "~/utils/api";
+
+import NotAuthenticated from "~/components/NotAuthenticated";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 
 export default function Profile() {
   const { status } = useSession();
@@ -13,8 +23,53 @@ export default function Profile() {
 
   return (
     <>
-      <div className="my-auto grid h-full grid-cols-2 place-items-center">
-        <pre>{JSON.stringify(userInfo, null, 2)}</pre>
+      <div className="grid grid-cols-4 gap-2 p-1">
+        <Card className="col-span-1 h-40">
+          <CardHeader className="flex flex-row items-center gap-2">
+            <Avatar>
+              <AvatarImage
+                src={userInfo ? (userInfo.user?.image as string) : ""}
+              />
+              <AvatarFallback>
+                {userInfo ? userInfo.user?.name?.split("")[0] : "Ic"}
+              </AvatarFallback>
+            </Avatar>
+            <div>
+              <CardTitle>{userInfo?.user?.name}</CardTitle>
+              <CardDescription>{userInfo?.user?.email}</CardDescription>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <p>Credits: {userInfo?.user?.credits}</p>
+          </CardContent>
+        </Card>
+        <section className="col-span-3">
+          <Card>
+            <CardHeader>
+              <CardTitle>Icons history</CardTitle>
+              <CardDescription>generated icons details</CardDescription>
+            </CardHeader>
+            <CardContent>
+              {userInfo?.userIcons.map((icon) => {
+                return (
+                  <div className="my-4">
+                    <div>
+                      <p>
+                        {icon.createdAt
+                          ? new Date(
+                              icon.createdAt.split(" ").join("T"),
+                            ).toLocaleString()
+                          : ""}
+                      </p>
+                      <p>{icon.prompt}</p>
+                      <p>{icon.id}</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </CardContent>
+          </Card>
+        </section>
       </div>
     </>
   );
