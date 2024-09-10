425c1142282e646831462218340af751ff8899fa
Sushant Sharma <sushant.wurk@gmail.com>
2024-09-10 19:34:29 +0530

    image tiling

--- src/pages/community.tsx
+++ src/pages/community.tsx
@@ -2,7 +2,7 @@ import { b64Image, b64Image2 } from "~/data/b64";
 
 export default function Community() {
   const imagesArr = new Array(20).fill(b64Image).map((_, idx) => {
-    if (idx % 2) return b64Image;
+    if (idx % 2 === 0 || idx % 3 === 0) return b64Image;
     else return b64Image2;
   });
   return (
